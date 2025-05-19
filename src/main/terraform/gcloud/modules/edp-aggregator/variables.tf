887e45df315ff300812d76bbb69f01182583d285
Matthias Georgi <mmg@meta.com>
2025-05-19 18:03:07 +0200

    use region from root module

--- src/main/terraform/gcloud/modules/edp-aggregator/variables.tf
+++ src/main/terraform/gcloud/modules/edp-aggregator/variables.tf
@@ -155,3 +155,9 @@ variable "nat_name" {
   type        = string
   default     = "nat-gateway"
 }
+
+variable "region" {
+  description = "The region where resources will be created."
+  type        = string
+  nullable    = false
+}
